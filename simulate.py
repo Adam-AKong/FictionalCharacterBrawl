db5400a84b2ab253a5ea9dee68a7d9c83c4a599e
BrKap <259brian952@gmail.com>
2025-06-01 00:45:46 -0700

    Removed old comments

--- simulate.py
+++ simulate.py
@@ -8,14 +8,10 @@ if __name__ == "__main__":
     character_total = 150_000 # There will be more inserts due to the many to many relationship with char/fran, 1-5 franchises per character
     review_total = 200_000 # Double this since it does franchise and character at the same time
     battle_total = 300_000 # There will be more inserts due to there being 0-10 votes per battle
-    # 1 million users
+
     user.simulate_users(user_total)
-    # 1 million franchises
     franchise.simulate_franchises(franchise_total)
-    # 1 million characters
     character.simulate_characters(character_total, user_total, franchise_total)
-    # 1 million reviews of both franchise and character
     review.simulate_reviews(review_total, user_total, character_total, franchise_total)
-    # 1 million battles along with voting
     battle.simulate_battles(battle_total, user_total, character_total)
     print("Simulation Complete")
